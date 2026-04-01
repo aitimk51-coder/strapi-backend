c689612112b71ffa5a2a0b20eef5f17fff5e681f
aitimk51-coder <ivanm1959@mail.ru>
2026-04-01 12:49:09 +0300

    fix: server.ts with correct types

---
+++ config/server.ts
@@ -0,0 +1,9 @@
+export default ({ env }: any) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 8080),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+  url: env('PUBLIC_URL', ''),
+  proxy: true,
+});
